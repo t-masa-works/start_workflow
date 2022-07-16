a764d23d7b99e02c447155b5c711dc9f78167d69
tei <tei952@hotmail.com>
2022-07-16 16:21:36 +0900

    modify by zj

--- src/main/java/com/gogoing/workflow/WorkflowApplication.java
+++ src/main/java/com/gogoing/workflow/WorkflowApplication.java
@@ -1,13 +1,17 @@
 package com.gogoing.workflow;
 
 import org.activiti.spring.boot.SecurityAutoConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication( exclude= SecurityAutoConfiguration.class)
+@MapperScan("boot.spring.mapper")
+@EnableScheduling
 public class WorkflowApplication {
 
-	public static void main(String[] args) {
+	public static void main_(String[] args) {
 		SpringApplication.run(WorkflowApplication.class, args);
 	}
 
